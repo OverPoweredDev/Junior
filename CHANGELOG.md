fd8b97c5437073898778bcd2cb28e0076cfd24a0
OverPoweredDev <omkar.prabhune19@vit.edu>
2021-12-07 20:07:37 +0530

    got everything ready (again) for `1.4.0`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -69,3 +69,19 @@ I got a lot of complaints of the app suddenly not showing any novels and only wo
 The Bug Report and Feature Request Links now don't need you to log in to Github
 
 Also note: we do have Volume support, just click on Volume in "Chapter/Volume Progress"
+
+## 1.4.0
+
+Sorry for the delay folks, Exam season where I live right now. Anyways, big update, this one. We got:
+
+𝗥𝗲𝗮𝗱𝗶𝗻𝗴 𝗟𝗶𝘀𝘁𝘀!!!
+All tags now double as reading lists. So you can group novels together by those if you want to. Detailed explanation (as with the others) in the FAQ's
+
+𝗗𝘆𝗻𝗮𝗺𝗶𝗰 𝗟𝗶𝗻𝗸𝘀
+Links are now dynamic! A lot of novel websites use a simple 𝘯𝘢𝘮𝘦.𝘤𝘰𝘮/𝘯𝘰𝘷𝘦𝘭/𝘤𝘩𝘢𝘱𝘵𝘦𝘳𝘕𝘶𝘮 format. So the basic idea is to automatically (dynamically) replace the 𝘤𝘩𝘢𝘱𝘵𝘦𝘳𝘕𝘶𝘮 part with your progress entered. Check the FAQ page for more details.
+
+𝗙𝗔𝗤 𝗣𝗮𝗴𝗲
+New FAQ page, down in the settings menu. Check it out! I'd actually love some design input on that, looks kinda plain to me...
+
+𝗡𝗼𝘃𝗲𝗹 𝗘𝘅𝗽𝗼𝗿𝘁 𝗜𝘀𝘀𝘂𝗲
+There's an issue with some folks regarding this setting. Check the FAQ page from Settings for that.
--- src/lib/faq_page/components/faq_list.dart
+++ src/lib/faq_page/components/faq_list.dart
@@ -15,7 +15,8 @@ const List<FAQCard> FAQList = [
     data: volumeSupportAnswer,
   ),
   FAQCard(
-    title: "Nothing happens when I press Export Novel Data, what's up with that?",
+    title:
+        "Nothing happens when I press Export Novel Data, what's up with that?",
     data: exportDataAnswer,
   ),
   FAQCard(
@@ -24,8 +25,7 @@ const List<FAQCard> FAQList = [
   ),
 ];
 
-const String dynamicLinksAnswer =
-"""A lot of novel websites use a simple
+const String dynamicLinksAnswer = """A lot of novel websites use a simple
 
 name.com/novel/chapterNum
 
@@ -42,7 +42,7 @@ Similarly, <num-6> would turn into .../chapter-003460. Not useful in this case b
 """;
 
 const String novelTagsAnswer =
-"""Alright Tags are easy enough to understand, they're just the tags or genres like say 'CN' you would assign to a novel. Apart from those, you can also tag a novel as part of your 'Favourites' or 'Top 10'.
+    """Alright Tags are easy enough to understand, they're just the tags or genres like say 'CN' you would assign to a novel. Apart from those, you can also tag a novel as part of your 'Favourites' or 'Top 10'.
 
 What the Reading Lists update does is it turns these tags into lists. That is, it'll automatically add all novels with the 'Favourites' tag to Reading List titled 'Favourites'. Naturally, it's the same for 'CN' or 'Top 10' or any other tag you have.
 
@@ -50,13 +50,13 @@ There's also a setting to disable these if you're not into it (Default is enable
 """;
 
 const String volumeSupportAnswer =
-"""Volume Support is there! Just click on 'Volume' in the Chapter/Volume Progress Heading while editing any Novel and it'll automatically open the volume text fields and take care of the presentation on the home page and all.
+    """Volume Support is there! Just click on 'Volume' in the Chapter/Volume Progress Heading while editing any Novel and it'll automatically open the volume text fields and take care of the presentation on the home page and all.
 
 As for why it's so hidden? I didn't wanna make the app a mess of buttons everywhere so it ended up being a little inconspicuous. I'm still very happy with how it looks honestly.
 """;
 
 const String exportDataAnswer =
-"""Some of you guys reported an Issue with Novel Exporting. i.e. when you press the export button, nothing happens. 
+    """Some of you guys reported an Issue with Novel Exporting. i.e. when you press the export button, nothing happens. 
 
 Big Thanks to Ana Lopes who told me how to fix it on Github! the issue can be solved by enabling the 'Install from Unknown Sources' setting. Basically it just doesn't let the app store anything otherwise.
 
@@ -64,7 +64,7 @@ I'll try to fix it soon but till then, this isn't Ideal but it solves the proble
 """;
 
 const String helpAnswer =
-"""Any and all contributions are very very welcome. Be it in adding features, spotting and fixing bugs or even just documentation, Junior has a long way to go.
+    """Any and all contributions are very very welcome. Be it in adding features, spotting and fixing bugs or even just documentation, Junior has a long way to go.
 
 There's a button in Settings to check out the source code, just go there and create an issue for whatever you feel the devs should know or if you wanna help with it yourself.
 """;
